7973d8a76a7be7fb756272ffed114f7107faf648
philippefang973 <philippe.fang973@gmail.com>
2024-02-16 08:42:50 +0100

    Prepare routers

--- dl_db.sh
+++ dl_db.sh
@@ -1,8 +1,10 @@
 mkdir datasets
-curl "https://zenodo.org/records/1289786/files/Orchset_dataset_0.zip?download=1" --output "datasets/orchset.zip"
-curl "https://qsdfo.github.io/LOP/database/LOP_database_06_09_17.zip" --output "datasets/lop.zip"
-curl "https://storage.googleapis.com/magentadata/datasets/maestro/v3.0.0/maestro-v3.0.0-midi.zip" --output "datasets/maestro.zip"
+cd datasets
+curl "https://zenodo.org/records/1289786/files/Orchset_dataset_0.zip?download=1" -O "orchset.zip"
+curl "https://qsdfo.github.io/LOP/database/LOP_database_06_09_17.zip" -O "lop.zip"
+curl "https://storage.googleapis.com/magentadata/datasets/maestro/v3.0.0/maestro-v3.0.0-midi.zip" -O "maestro.zip"
 
-unzip "datasets/orchset.zip"
-unzip "datasets/lop.zip"
-unzip "datasets/maestro.zip"
+unzip "orchset.zip"
+unzip "lop.zip"
+unzip "maestro.zip"
+cd ..
--- src/backend/app.py
+++ src/backend/app.py
@@ -1,10 +1,65 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
-@app.route('/')
+@app.route('/',methods=['POST'])
 def get_data():
     data = {
-        'title': 'Welcome to Angular-Python App',
-        'message': 'This is an example integration between Angular and Python!'
+        'title': 'Welcome to Orchestral AI',
+        'message': 'Homepage'
+    }
+    return jsonify(data)
+
+@app.route('/signin',methods=['POST'])
+def get_data():
+    req = request.json
+    #Show form
+    if not req :
+        data = {
+            'title': 'Sign In',
+            'message': 'form'
+        }
+        return jsonify(data)
+    #Form validation
+    else :
+        return jsonify({})
+
+@app.route('/signup',methods=['POST'])
+def get_data():
+    req = request.json
+    #Show form
+    if not req :
+        data = {
+            'title': 'Sign Up',
+            'message': 'form'
+        }
+        return jsonify(data)
+    #Form validation
+    else :
+        return jsonify({})
+    
+@app.route('/dashboard',methods=['POST'])
+def get_data():
+    req = request.json
+    data = {
+        'title': 'Hello',
+        'message': 'dashboard'
+    }
+    return jsonify(data)
+
+@app.route('/infos',methods=['POST'])
+def get_data():
+    req = request.json
+    data = {
+        'title': 'Personal information',
+        'message': ''
+    }
+    return jsonify(data)
+
+@app.route('/prediction',methods=['POST'])
+def get_data():
+    req = request.json
+    data = {
+        'title': 'Process music',
+        'message': ''
     }
     return jsonify(data)
 
