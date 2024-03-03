617fcfd032194a9626aee387b4e5f4238d731b01
philippefang973 <philippe.fang973@gmail.com>
2024-03-03 21:05:24 +0100

    kubernetes frontend with nginx succesful, need to fix flask cors when deployed

--- src/backend/app/app.py
+++ src/backend/app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+import requests
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": ["http://localhost:4200", "http://angular.default.svc.cluster.local:4200"]}})
 
